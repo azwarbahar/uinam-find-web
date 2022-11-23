bfe88f522aaeb46389fa31177b98316d49d05cc3
azwarbahar <azwarbahar07@gmail.com>
2022-11-24 03:36:37 +0800

    jfkldgjlk

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Makassar");
-$conn = mysqli_connect("localhost", "root", "", "uinamfind_db");
-// $conn = mysqli_connect("localhost", "u3737783_uinamfind", "uinamfind", "u3737783_uinamfind_db");
+// $conn = mysqli_connect("localhost", "root", "", "uinamfind_db");
+$conn = mysqli_connect("localhost", "u3737783_uinamfind", "uinamfind", "u3737783_uinamfind_db");
 ?>
 <!-- again -->
 <?php
